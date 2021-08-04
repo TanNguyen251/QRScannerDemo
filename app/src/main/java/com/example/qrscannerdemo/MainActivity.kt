11ca4dfe0e13dca7f17fb53ed6234638abaafe79
Tan Nguyen <tan.nguyen.9225.1@gmail.com>
2021-08-04 08:36:08 -0700

    update

--- app/src/main/java/com/example/qrscannerdemo/MainActivity.kt
+++ app/src/main/java/com/example/qrscannerdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest.permission.*
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.SurfaceHolder
+import android.view.SurfaceHolder.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -20,29 +21,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var cameraSource: CameraSource
     lateinit var binding: ActivityMainBinding
 
-    private val surfaceCallBack = object : SurfaceHolder.Callback {
-
-        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        }
-
-        override fun surfaceDestroyed(holder: SurfaceHolder) {
-            cameraSource.stop()
-        }
-
-        override fun surfaceCreated(holder: SurfaceHolder) {
-            // check camera permission for api version 23
-            if(ContextCompat.checkSelfPermission(this@MainActivity, CAMERA)== PackageManager.PERMISSION_GRANTED )
-                cameraSource.start(holder)
-            else requestPermissions( arrayOf(CAMERA),1001)
-        }
-
-    }
+    lateinit var surfaceCallBack: Callback
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        init()
+
     }
 
     private fun init(){
@@ -55,18 +40,40 @@ class MainActivity : AppCompatActivity() {
                 if (detections.detectedItems.isNotEmpty()) {
                     val barcode = detections.detectedItems
                     if (barcode?.size() ?: 0 > 0) {
+                        binding.textScanResult.text = barcode.valueAt(0).displayValue
                         // show barcode content value
-                        Toast.makeText(this@MainActivity,  barcode?.valueAt(0)?.displayValue ?: "", Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(this@MainActivity,  barcode?.valueAt(0)?.displayValue ?: "", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
+
+        createSurfaceCallback()
         barcodeDetector.setProcessor(processor)
 
         cameraSource = CameraSource.Builder(applicationContext,barcodeDetector)
-            .setRequestedFps(25f)
             .setAutoFocusEnabled(true).build()
         binding.cameraSurfaceView.holder.addCallback(surfaceCallBack)
+    }
+
+    private fun createSurfaceCallback(){
+        surfaceCallBack = object : Callback {
+
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                // check camera permission for api version 23
+                if(ContextCompat.checkSelfPermission(this@MainActivity, CAMERA)== PackageManager.PERMISSION_GRANTED )
+                    cameraSource.start(holder)
+                else requestPermissions( arrayOf(CAMERA),1001)
+            }
+
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -88,6 +95,11 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        init()
     }
 
     override fun onDestroy() {
